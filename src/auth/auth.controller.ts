051b2704c4aaffa06b98d475036884d567141111
Calvin Ling <calvinling8@gmail.com>
2021-05-16 11:54:04 -0400

    Add an endpoint for verifying tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Throttle } from '@nestjs/throttler';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
+@Throttle(
+  +process.env.AUTHENTICATION_THROTTLE_LIMIT,
+  +process.env.AUTHENTICATION_THROTTLE_TTL,
+)
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private jwt: JwtService) {}
 
   @Post('access-tokens')
-  @Throttle(5, 30)
   @UseGuards(GoogleAuthGuard)
   async googleAuth(@Req() req, @Res({ passthrough: true }) res: Response) {
     const accessToken = await this.authService.loginOrRegister(req.user);
@@ -18,6 +23,12 @@ export class AuthController {
       secure: true,
       sameSite: true,
     });
-    return accessToken;
+    return { accessToken, decodedAccessToken: this.jwt.decode(accessToken) };
+  }
+
+  @Get('token-info')
+  @UseGuards(JwtAuthGuard)
+  verifyAccessToken(@Req() { user }: Request) {
+    return { sub: user.id };
   }
 }
